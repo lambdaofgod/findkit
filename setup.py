affb9d68f7f0e83b07258328d35fe940e52201fc
kuba <bartczukkuba@gmail.com>
2018-06-17 13:12:14 +0200

    project setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='findkit',
+    version='0.1',
+    description='A Python library for content-based information retrieval',
+    url='https://github.com/lambdaofgod/findkit',
+    author='Jakub Bartczuk',
+    py_modules=['findkit']
+)
