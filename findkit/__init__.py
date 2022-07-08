cb67618e2714e8424c7229cbf687cc2b82e28037
lambdaofgod <bartczukkuba@gmail.com>
2022-07-08 14:02:43 +0200

    added base classes to toplevel for more convenient import

--- findkit/__init__.py
+++ findkit/__init__.py
@@ -1,3 +1,5 @@
 from . import feature_extractor
 from . import index
 from . import vector_loader
+from .feature_extractor.feature_extractor import FeatureExtractor 
+from .index.index import Index 
