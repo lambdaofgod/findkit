4b879df8bff96e9d1c563f41a7fb6fb9e50cd84e
lambdaofgod <bartczukkuba@gmail.com>
2022-11-20 20:08:05 +0100

    allow retrieval pipeline with different feature extractor for queries

--- findkit/retrieval_pipeline.py
+++ findkit/retrieval_pipeline.py
@@ -3,19 +3,29 @@ from findkit.index import Index
 from findkit.feature_extractor import FeatureExtractor
 from dataclasses import dataclass
 from findkit import serialization
-from typing import Callable
+from typing import Callable, Optional
 
 
-@dataclass
 class RetrievalPipelineFactory:
-    feature_extractor: FeatureExtractor
-    index_factory: Callable[[np.ndarray], Index]
+    def __init__(
+        self,
+        feature_extractor: FeatureExtractor,
+        index_factory: Callable[[np.ndarray], Index],
+        query_feature_extractor: Optional[FeatureExtractor] = None,
+    ):
+        self.feature_extractor = feature_extractor
+        self.index_factory = index_factory
+        self.query_feature_extractor = (
+            query_feature_extractor
+            if query_feature_extractor is not None
+            else feature_extractor
+        )
 
     def build(self, data, **kwargs):
         index = self.index_factory(
             self.feature_extractor.extract_features(data), **kwargs
         )
-        return RetrievalPipeline(index=index, feature_extractor=self.feature_extractor)
+        return RetrievalPipeline(index=index, feature_extractor=self.feature_extractor, self.query_feature_extractor)
 
     @classmethod
     def from_config(cls, config: serialization.PipelineConfig):
@@ -29,7 +39,8 @@ class RetrievalPipeline:
 
     feature_extractor: FeatureExtractor
     index: Index
+    query_feature_extractor: FeatureExtractor
 
     def find_similar(self, query, n_returned: int):
-        query_obj = self.feature_extractor.extract_features(query)
+        query_obj = self.query_feature_extractor.extract_features(query)
         return self.index.find_similar(query_obj, n_returned)
